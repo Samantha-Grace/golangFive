b9f8cb1df5d47a8648d3b701eaa54f812aebbac4
Samantha-Grace <sgrace732@gmail.com>
2021-08-09 14:09:23 -0600

    conditionals practice

--- conditionals/main.go
+++ conditionals/main.go
@@ -10,4 +10,17 @@ func main() {
 	if x > 5 {
 		fmt.Println("x is big")
 	}
+
+	if x > 100 {
+		fmt.Println("x is big")
+	} else {
+		fmt.Println("x is not that big")
+	}
+
+	if x > 5 && x < 15 {
+		fmt.Println("x is just right")
+	}
+	if x > 20 || x < 30 {
+		fmt.Println("x is out of range")
+	}
 }
