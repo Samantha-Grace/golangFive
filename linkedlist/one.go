de98d1afbba5d5da8f8bf52bffd24e0aafa402a6
Samantha-Grace <sgrace732@gmail.com>
2022-01-20 13:50:43 -0700

    Cody going over pinters

---
+++ linkedlist/one.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// type LinkedList struct {
+// 	num *int
+// }
+
+// func (ll LinkedList) add(i int) {
+// 	*ll.num = i
+// }
+
+// func main() {
+// 	num := 7
+// 	ll := LinkedList{num: &num}
+// 	fmt.Println(*ll.num)
+// 	ll.add(4)
+// 	fmt.Println(*ll.num)
+// }
