b049909837645285dd7c64c5a2d67ddf83d43f73
Samantha-Grace <sgrace732@gmail.com>
2022-02-07 14:46:26 -0700

    moving this crap over to make room for practice

---
+++ maps/main2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func maintwo() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("[english word] -> [turkish word]")
+		return
+	}
+	query := args[0]
+
+	dict := map[string]string{
+		"good":    "kotu",
+		"great":   "harika",
+		"perfect": "mukemmel",
+	}
+
+	dict["up"] = "yukari"
+	dict["down"] = "asagi"
+	dict["good"] = "iyi"
+	dict["mistake"] = ""
+
+	// copied := map[string]string{"down":"asagi", "good":"iyi", "great":"harika", "mistake":"", "perfect":"mukemmel", "up":"yukari"}
+	// "perfect" means "mukemmel"
+
+	// fmt.Printf("%#v\n", dict)
+	// for k, v := range dict {
+	// 	fmt.Printf("%q means %#v\n", k, v)
+	// }
+
+	if value, ok := dict[query]; ok {
+		fmt.Printf("%q means %#v\n", query, value)
+		return
+	}
+	fmt.Printf("%q not found.\n", query)
+
+	value := dict[query]
+	fmt.Printf("%q means %#v\n)", query, value)
+	fmt.Printf("# of keys: %d\n", len(dict))
+}
