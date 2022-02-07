74c4697809009c0932bf6ab8c2c663cb06a78de5
Samantha-Grace <sgrace732@gmail.com>
2022-02-07 09:31:04 -0700

    going over aggregate types

--- aggregate/main.go
+++ aggregate/main.go
@@ -1,6 +1,74 @@
 // // DC udem go prog lang crash course
 package main
 
+import "fmt"
+
+// struct is an aggregate type because it holds many pieces of information
+
+//declare a type, we'll call is Car, and it is a struct
+type Car struct {
+	// describe the kind of info it holds
+	NumberofTires int
+	Luxury        bool
+	Bucketseats   bool
+	Make          string
+	Model         string
+	Year          int
+}
+
+// func main() {
+// 	// create a variable of that type
+// 	var myCar Car
+
+// 	myCar.NumberofTires = 4
+// 	myCar.Luxury = false
+// 	myCar.Bucketseats = true
+// 	myCar.Make = "Ford"
+// 	myCar.Model = "Mustang"
+// 	myCar.Year = 1969
+
+// }
+
+// shortahnd for declaring and populating a variable
+func main() {
+	myCar := Car{
+		NumberofTires: 4,
+		Luxury:        false,
+		Bucketseats:   true,
+		Make:          "Ford",
+		Model:         "Mustang",
+		Year:          1969,
+	}
+	fmt.Printf("My cars is a %d %s %s", myCar.Year, myCar.Make, myCar.Model)
+}
+
+// import "fmt"
+
+// //create an array
+// func main() {
+// 	// create a new variable that is an array with a length of three and type string
+// 	var myStrings [3]string
+// 	// we can assign values to the array - arrays count from 0
+// 	myStrings[0] = "cat"
+// 	myStrings[1] = "dog"
+// 	myStrings[2] = "bird"
+
+// 	fmt.Println("First is:", myStrings[0])
+// }
+
+//  you will prob use slices more than arrays in go
+// //create an array of ints
+// func main() {
+// 	// create a new variable that is an array with a length of three and type int
+// 	var myStrings [3]string
+// 	// we can assign values to the array - arrays count from 0
+// 	myStrings[0] = 1
+// 	myStrings[1] = 2
+// 	myStrings[2] = 3
+
+// 	fmt.Println("First is:", myStrings[0])
+// }
+
 // // aggregate types (assar, struct)
 // // reference types (pointers, slices, maps, functions, channels)
 // // interface type
