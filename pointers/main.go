ab1b411c9a46d7ed31ccdc1938841d704de4fefc
Samantha-Grace <sgrace732@gmail.com>
2022-02-07 11:16:30 -0700

    dereference

--- pointers/main.go
+++ pointers/main.go
@@ -6,10 +6,16 @@ import "fmt"
 func main() {
 	x := 10
 
-	myFirstPointer := &x
+	myFirstPointer := &x // & holdw the address of whrere x's value of 10 is stored in memory
 
 	fmt.Println("x is", x)                             // prints out: x is 10
 	fmt.Println("My first pointer is", myFirstPointer) // prints out: My first pointer is 0xc00009e240
+
+	// go to the address in myFirstPointer, and change the contents of that memory location to 20
+	*myFirstPointer = 20 // * dereferences the pointer and sets the value of x to 20
+
+	fmt.Println("X is now", x) // prints out: X is now 20
+
 }
 
 // func main() {
